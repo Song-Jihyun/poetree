54fbedb686b049065cbd201cc08d035ab8667b4f
Song-Jihyun <thdwlgus0820@gmail.com>
2020-11-01 18:31:13 +0900

    change file name front to frontend

--- front/javascript/sidebar.js
+++
@@ -1,9 +0,0 @@
-function sidebar_open() {
-    document.getElementsByTagName("aside")[0].style.display = "flex";
-    document.getElementById("sidebar_toggle").setAttribute("onClick", "sidebar_close()");
-}
-
-function sidebar_close() {
-    document.getElementsByTagName("aside")[0].style.display = "none";
-    document.getElementById("sidebar_toggle").setAttribute("onClick", "sidebar_open()");
-}
